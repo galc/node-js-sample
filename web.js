b42424dc87830d53460c8f6bcf3969d2a66cd3e0
Ubuntu <ubuntu@ip-172-31-45-191.us-west-2.compute.internal>
2013-07-11 19:48:58 +0000

    Updated web.js to use value from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express 	= require('express');
+var fs		= required('fs');
 
 var app = express.createServer(express.logger());
+var text = "";
+
+fs.readfile('index.html', function (err,data){
+	if (err) throw err;
+	console.log(data);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
